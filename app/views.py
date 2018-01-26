d7a3b15a513204afe232b5275596969fc3130838
Mulberry <wowcxy2008@126.com>
2018-01-26 15:38:18 +0800

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,10 +5,20 @@ from django.conf import settings
 
 def robot (request):
   context = {}
-  context["env"] = settings.ENV
+  context["env"] = settings.PY_ENV
   return render(request, "robot.html", context)
 
 def talk (request):
   context = {}
-  context["env"] = settings.ENV
+  context["env"] = settings.PY_ENV
   return render(request, "talk.html", context)
+
+def admin_login (request):
+  context = {}
+  context["env"] = settings.PY_ENV
+  return render(request, "admin/login.html", context)
+
+def admin (request):
+  context = {}
+  context["env"] = settings.PY_ENV
+  return render(request, "admin/index.html", context)
