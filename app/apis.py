f276eec6eec098c38039a110479f3291d5829b3d
Mulberry <wowcxy2008@126.com>
2018-01-26 15:37:35 +0800

    Update apis.py

--- app/apis.py
+++ app/apis.py
@@ -1,24 +1,29 @@
-# -*- coding: utf-8 -*-
-#from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 import json
-import urllib, sys
+import urllib.request
 import ssl
+from django.core.paginator import Paginator
+from django.contrib.auth import login as authorize
+from django.contrib.auth import logout as authorize_logout
+
+from robot.models import Type
+from robot.models import ChatRecord
+from robot.models import User
 
 def getWeather (request):
-  ip = request.GET['ip'];
-  host = 'https://ali-weather.showapi.com'
-  path = '/ip-to-weather'
-  method = 'GET'
-  appcode = '7be8f8f70a2d47b88d3d1ab5bd9b2e8d'
-  querys = 'ip=' + ip + '&need3HourForcast=0&needAlarm=0&needHourData=0&needIndex=0&needMoreDay=0'
+  ip = request.GET["ip"]
+  host = "https://ali-weather.showapi.com"
+  path = "/ip-to-weather"
+  method = "GET"
+  appcode = "7be8f8f70a2d47b88d3d1ab5bd9b2e8d"
+  querys = "ip=" + ip + "&need3HourForcast=0&needAlarm=0&needHourData=0&needIndex=0&needMoreDay=0"
   bodys = {}
-  url = host + path + '?' + querys
+  url = host + path + "?" + querys
   context = ssl._create_unverified_context()
 
   _request = urllib.request.Request(url)
-  _request.add_header('Authorization', 'APPCODE ' + appcode)
+  _request.add_header("Authorization", "APPCODE " + appcode)
 
   with urllib.request.urlopen(_request, context=context) as response:
     data = response.read()
@@ -26,47 +31,181 @@ def getWeather (request):
     reason = response.reason
     
   if status == 200:
-    return HttpResponse(data.decode('utf-8'), content_type='application/json; charset=utf-8')
+    return HttpResponse(data.decode("utf-8"), content_type="application/json; charset=utf-8")
   else:
-    return HttpResponse(reason)
+    response = {
+      "reason": reason,
+    }
+    
+    return HttpResponseServerError(reason, content_type="application/json; charset=utf-8")
   
-def test (request):
-  # 初始化
-  response = ""
-  response1 = ""
-  
-  # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
-  list = Test.objects.all()
+def getTypes (request):
+  per_page = request.GET["per_page"]
+  curr_page = request.GET["curr_page"]
+  name = request.GET["name"]
+
+  types = Types.objects.filter(name__contains = name)
+  paginator = Paginator(types, per_page)
       
-  # filter相当于SQL中的WHERE，可设置条件过滤结果
-  response2 = Test.objects.filter(id=1) 
+  response = {
+    "list": paginator.page(curr_page),
+    "per_page": per_page,
+    "curr_page": curr_page,
+    "count": paginator.count,
+    "name": name
+  }
   
-  # 获取单个对象
-  response3 = Test.objects.get(id=1) 
-  
-  # 限制返回的数据 相当于 SQL 中的 OFFSET 0 LIMIT 2;
-  Test.objects.order_by('name')[0:2]
-  
-  #数据排序
-  Test.objects.order_by("id")
-  
-  # 上面的方法可以连锁使用
-  Test.objects.filter(name="runoob").order_by("id")
-  
-  # 输出所有数据
-  for var in list:
-      response1 += var.name + " "
-  response = response1
-  return HttpResponse("<p>" + response + "</p>")
+  return HttpResponse(response)
 
-def messageList (request):
-  return HttpResponse("<p>" + response + "</p>")
+def addType (request):
+  name = request.POST["name"]
+  keywords = request.POST["keywords"]
 
-def messageAdd (request):
-  return HttpResponse("<p>" + response + "</p>")
+  if name == None:
+    response = {
+      "errorMsg": "类型名称不可为空",
+    }
 
-def messageDelete (request):
-  return HttpResponse("<p>" + response + "</p>")
+    return HttpResponseNotAllowed(response)
 
-def talk (request):
-  return HttpResponse("<p>" + response + "</p>")
+  if keywords == None:
+    keywords = ""
+
+  add = Type(name = name, keywords = keywords)
+  add.save()
+  add.id
+
+  response = {
+    id: add.id
+  }
+
+  return HttpResponse(response)
+
+def updateType (request):
+  id = request.POST["id"]
+  name = request.POST["name"]
+  keywords = request.POST["keywords"]
+
+  if id == None:
+    response = {
+      errorMsg: "类型id不可为空"
+    }
+
+    return HttpResponseNotAllowed(response)
+
+  if name == None:
+    response = {
+      errorMsg: "类型名称不可为空"
+    }
+
+    return HttpResponseNotAllowed(response)
+
+  type = Type.objects.get(id = id)
+  type.name = name
+  type.keywords = keywords
+  type.save()
+
+  response = {
+    "msg": "更新类型成功"
+  }
+
+  return HttpResponse(response)
+
+def deleteTypes (request):
+  ids = request.POST["ids"]
+
+  if ids == None:
+    response = {
+      "errorMsg": "ids不可为空"
+    }
+
+    return HttpResponseNotAllowed(response)
+
+  for id in ids:
+    Type.objects.filter(id = id).delete()
+
+  response = {
+    "msg": "删除成功"
+  }
+
+  return HttpResponse(response)
+
+
+
+def getChatRecords (request):
+  per_page = request.GET["per_page"]
+  curr_page = request.GET["curr_page"]
+  type = request.GET["type"]
+  start_at = request.GET["start_at"]
+  end_at = request.GET["end_at"]
+
+  types = ChatRecord.objects.filter(type__contains = type).filter(create_at__gte = start_at).filter(type__lte = end_at)
+  paginator = Paginator(types, per_page)
+      
+  response = {
+    "list": paginator.page(curr_page),
+    "per_page": per_page,
+    "curr_page": curr_page,
+    "count": paginator.count,
+    "name": name
+  }
+  
+  return HttpResponse(response)
+
+def sendMessage (request):
+  id = request.META["REMOTE_ADDR"]
+  content = request.POST["content"]
+
+  if content == None:
+    response = {
+      "errorMsg": "消息内容不可为空",
+    }
+
+    return HttpResponseNotAllowed(response)
+
+  add = ChatRecord(id = id, content = content)
+  add.save()
+  add.id
+
+  response = {
+    id: add.id
+  }
+
+  return HttpResponse(response)
+
+def login (request):
+  username = request.POST["username"]
+  password = request.POST["password"]
+
+  user = User.objects.get(username = username)
+
+  if user == None:
+    response = {
+      errorMsg: "用户名错误"
+    }
+
+    HttpResponseNotFound(response)
+
+  if user.password == password:
+    response = {
+      errorMsg: "密码错误错误"
+    }
+
+    HttpResponseNotFound(response)
+
+  authorize(request, user)
+
+  response = {
+    msg: "登录成功"
+  }
+
+  return HttpResponse(response)
+
+def logout (request):
+  authorize_logout(request)
+
+  response = {
+    msg: "登出成功"
+  }
+
+  return HttpResponse(response)
