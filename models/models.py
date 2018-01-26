be38e67904f5df7b5c56535bd0afa3d4ee0a1b41
Mulberry <wowcxy2008@126.com>
2018-01-26 15:36:48 +0800

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,5 +1,37 @@
-# models.py
 from django.db import models
- 
-class user(models.Model):
-  name = models.CharField(max_length=20)
+import django.utils.timezone as timezone
+
+# 消息类型
+class Type (models.Model):
+  # 类型名称
+  name = models.CharField(max_length = 20, null = False)
+
+  # keywords
+  keywords = models.CharField(max_length = 80, null = True)
+
+# 聊天记录
+class ChatRecord(models.Model):
+  # 聊天者的ip
+  ip = models.CharField(max_length = 15, null = False)
+
+  # 消息内容
+  content = models.CharField(max_length = 80, null = False)
+
+  # 消息类型
+  type = models.ForeignKey(Type, on_delete = models.DO_NOTHING, null = True)
+
+  # 保存日期
+  create_at = models.DateTimeField(default = timezone.now)
+  
+  # 最后修改日期
+  update_at = models.DateTimeField(auto_now = True)
+
+class User (models.Model):
+  # 用户名
+  username = models.CharField(max_length = 20)
+
+  # 密码
+  password = models.CharField(max_length = 20)
+
+  # email
+  email = models.CharField(max_length = 35)
