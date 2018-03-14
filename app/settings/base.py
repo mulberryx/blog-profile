ce3c2b395c52864efba652b3d0713b87fd1bc465
Tea Muffin <wowcxy2008@126.com>
2018-03-14 15:11:03 +0800

    Update base.py

--- app/settings/base.py
+++ app/settings/base.py
@@ -10,9 +10,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-  "django.contrib.auth",
-  "django.contrib.contenttypes",
-  "django.contrib.sessions",
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
   "django.contrib.messages",
   "robot",
 ]
