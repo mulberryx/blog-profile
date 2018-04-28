fbc45e2afd7ff1282c19f0ca4eb0ad1d660fc850
Steudnera Taro <wowcxy2008@126.com>
2018-04-28 17:31:16 +0800

    Update optimal-solution.py

--- app/services/gobang/optimal-solution.py
+++ app/services/gobang/optimal-solution.py
@@ -3,11 +3,11 @@
 from Checkerboard import Checkerboard
 checkerboard = Checkerboard()
 
-/**
+"""
  * 获取机器方最优解
  * @param {Piece[]} 当前棋子列表
  * @return {Piece} 机器方最优解棋子
- */
+"""
 def getOptimalSolution (Pieces)
   checkerboard.sync(Pieces)
   return checkerboard.getOptimalPiece()
